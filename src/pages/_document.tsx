d59c3ba068d784f69b2ceccca5ea0eff5cda32cb
KAS-O <kaso.kontakt@gmail.com>
2025-10-16 05:42:13 +0200

    Add files via upload

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+// src/pages/_document.tsx
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="pl">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#b68f4a" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
