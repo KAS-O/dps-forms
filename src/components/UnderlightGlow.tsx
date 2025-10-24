7bad8108f79942c9dadbc12bf43605a56f97bb7e
KAS-O <kaso.kontakt@gmail.com>
2025-10-25 00:16:04 +0200

    Create UnderlightGlow.tsx

---
+++ src/components/UnderlightGlow.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function UnderlightGlow() {
+  return (
+    <div className="underlight-ambient" aria-hidden="true">
+      <span className="underlight-ambient__blue" />
+      <span className="underlight-ambient__red" />
+    </div>
+  );
+}
+
+export default UnderlightGlow;
