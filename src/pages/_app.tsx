98ed382fbae76c26237916826146bac8d638112b
KAS-O <kaso.kontakt@gmail.com>
2025-11-17 00:47:55 +0100

    Set default viewport scale to 80%

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <ActivityLoggerProvider>
         <>
           <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="viewport" content="width=device-width, initial-scale=0.8" />
           </Head>
           <div className="min-h-screen flex flex-col relative bg-[#02060f] text-white">
             <UnderlightGlow />
